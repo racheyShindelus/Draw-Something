568c9b9a96d17401ebc711d8c69cf212f214f26a
Rachel F Shindelus <145309467+racheyShindelus@users.noreply.github.com>
2024-03-09 20:17:45 -0800

    Add files via upload

---
+++ DrawSomethingAssignment1Module2_Shindelus.java
@@ -0,0 +1,39 @@
+/*
+I believe my grade for this assingment should be 100 as I have satisifed all the requirements. 
+*/
+
+public class DrawSomethingAssignment1Module2_Shindelus   // Define class and name file
+{
+   public static void main(String[] args)       // Define main method with no return value
+   {
+         // Begining of hat
+      System.out.println("\t\t\t\t\t\t\t\t  *");      // "System.out.println" prints the text within the quotation marks then moves to the next line 
+      System.out.println("\t\t\t\t\t\t\t\t / \\");    // 8x "\t" are 8 indentations, "\\" produces a single backslash
+      System.out.println("\t\t\t\t\t\t\t   /   \\"); 
+      System.out.println("\t\t\t\t\t\t\t  /  \t  \\"); 
+      System.out.println("\t\t\t\t\t\t    /  \t   \\"); 
+      System.out.println("\t\t\t\t\t\t   /  \t\t \\"); 
+      System.out.println("\t\t\t\t\t\t  / ////\\\\\\\\  \\"); // Make some bangs (needs 2x as many backslashes written out to produce the desired amount)
+      System.out.println("\t\t\t\t\t    / \t__  __ \t\\");     
+         // Begining of face, end of hat
+      System.out.println("\t\t\t\t\t   | \t    @   @ \t |");  // Eyes
+      System.out.println("\t\t\t\t\t   | \t      $   \t |");  // Nose
+      System.out.println("\t\t\t\t\t   | \t     <_>  \t |");  // Mouth
+      System.out.println("\t\t\t ________|_______________|________"); 
+         // Begining of Torso, end of face
+      System.out.println("\t\t   / \t\t\t\t\t\t\t\t\t\t\t \\");
+      System.out.println("\t\t  / \t\t __\t __\t __\t __\t\t  \\");
+      System.out.println("\t\t / \t\t|J |\t|A |\t|V |\t|A |\t\t\t\\");     // "JAVA" pattern printed on torso
+      System.out.println("\t   / \t\t\t|__|\t|__|\t|__|\t|__|\t\t\t \\");
+      System.out.println("\t  / \t\t\t\t\t\t\t\t\t\t\t\t\t  \\");
+      System.out.println("\t /__________@@@________________@@@___________\\");
+         // Begining of legs, end of torso
+      System.out.println("\t\t\t\t\t@@@\t\t\t\t\t @@@");
+      System.out.println("\t\t\t\t\t@@@\t\t\t\t\t @@@");
+      System.out.println("\t\t\t\t\t@@@\t\t\t\t\t @@@");
+      System.out.println("\t\t\t\t\t@@@\t\t\t\t\t @@@");
+      System.out.println("\t\t\t  ____|\t\t\t\t\t\t\t|____");
+      System.out.println("\t\t\t |____| \t\t\t\t\t\t\t|____|");
+      System.out.println("\t\t\t\t/\\ \t\t\t\t\t\t\t  /\\");
+   }
+}
